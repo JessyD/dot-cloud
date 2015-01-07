7a24e2c8ed2fbb1740c6e92ae4e412e373c9e3ac
Alessandro Angelino <alessandro.angelino91@gmail.com>
2015-01-07 13:28:53 +0000

    python script basic implementatio updated

--- vim-cloud.py
+++ vim-cloud.py
@@ -1,12 +1,53 @@
+# modules
 import argparse
+import os
+import sys
+import shutil
+import time
 
-def install_vimrc():
+# current platform
+from sys import platform as _platform
+
+# supported platforms
+supported_platforms = ["linux", "darwin"]
+
+# default configuration
+home_path = os.path.expanduser("~")
+vimrc_file_path = home_path + "/.vimrc"
+vim_dir_path = home_path + "/.vim"
+
+# timestamp
+time_stamp = "_" + time.strftime('%Y%m%d%H%M%S', time.localtime())
+
+"""
+    install_vimrc(args)
+        it backs up a previous custom configuration of vim, if any, installs
+        all missing dependencies and then installs the current vim; the user
+        can also choose to install once, unlinking the .vimrc file from the
+        cloned git repository
+"""
+def install_vimrc(args):
+    if os.path.isdir(vim_dir_path):
+        print("Back up: " + vim_dir_path)
+        try:
+            shutil.copytree(vim_dir_path, vim_dir_path + time_stamp)
+        except RuntimeError:
+            print("Error while backing up " + vim_dir_path)
+    if os.path.isfile(vimrc_file_path):
+        print("Back up: " + vimrc_file_path)
+        try:
+            shutil.copyfile(vimrc_file_path, vimrc_file_path + time_stamp)
+        except RuntimeError:
+            print("Error while backing up " + vimrc_file_path)
+    print("Checking dependencies...")
+    if args.once:
+        print("Once")
     print("i")
 
-def update_vimrc():
+def update_vimrc(args):
     print("u")
 
-def clean_vimrc():
+def clean_vimrc(args):
     print("c")
 
 def switch(mode):
@@ -16,17 +57,19 @@ def switch(mode):
         'c': clean_vimrc,
     }[mode]
 
-def main():
+if __name__ == "__main__":
     # parse arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("-m", "--mode",
                         help="Execution mode: (i)nstall, (u)pdate, (c)lean",
                         type=str, action="store",
                         choices=["i", "u", "c"], default="i")
+    parser.add_argument("-o", "--once",
+                        help="Perform unlinked installation (static .vimrc)")
     args = parser.parse_args()
 
-    # execute the function corresponding to the chosen mode
-    switch(args.mode)()
+    if _platform not in supported_platforms:
+        raise RuntimeError("This platform is currently unsupported")
 
-if __name__ == "__main__":
-    main()
+    # execute the function corresponding to the chosen mode
+    switch(args.mode)(args)
